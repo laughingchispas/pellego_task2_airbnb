762e55710c9d70a7d87906d803b1b8f8c104a99c
Nicole Miller <laughingchispas@gmail.com>
2016-07-24 21:54:34 +0200

    calendar, footer adder

--- scripts/angularApp.js
+++ scripts/angularApp.js
@@ -17,12 +17,18 @@ myApp.controller('airbnbController', function($scope) {
 
 
     $scope.properties = [
-          {description: 'Private flat, with parking and yard', city: 'San Francisco', state: 'CA', country: 'USA', type: 'Entire home/apt', guests:'4', bedrooms: '1', beds:'2', reviews:'74'},
+          {description: 'Private flat, with parking and yard', city: 'San Francisco', state: 'CA', country: 'USA', type: 'Entire home/apt', guests:'4', bedrooms: '1', beds:'2', reviews:'74', price: '$139', baths:'1'},
         ];
 
 
     $scope.likes = [
         'cozy', 'residential', 'close to public transport', 'quiet', 'walkable'
+        ]
+
+    $scope.comments = [
+        'Beautiful 1 bedroom, 1 bathroom private apartment. Private entrance on side. Free off-street parking.',
+        'Brand new remodeled bathroom',
+        '1 block from subway and lots restaurants/bar walking distance'
         ]
 
 });
