de2dd01223b38d82c64f2c50adabda67f4c50f6e
Nicole Miller <laughingchispas@gmail.com>
2016-07-27 18:53:54 +0200

    Middle section formatting, adjustments of <p> tags, AngularJS for icons

--- scripts/angularApp.js
+++ scripts/angularApp.js
@@ -11,34 +11,64 @@ myApp.controller('airbnbController', function($scope) {
     ];
 
 
-    $scope.users = [
-      {image: 'image', name: 'Yann', bedrooms: 'search'}
-    ];
+    $scope.users = {
+      image: 'image',
+      name: 'Yann',
+      bedrooms: 'search'
+    };
 
 
-    $scope.properties = [
-          {description: 'Private flat, with parking and yard', city: 'San Francisco', state: 'CA', country: 'USA', type: 'Entire home/apt', guests:'4', bedrooms: '1', beds:'2', reviews:'74', price: '$139', baths:'1', saves: '634'}
-        ];
+    $scope.properties = {
+      description: 'Private flat, with parking and yard',
+      city: 'San Francisco',
+      state: 'CA',
+      country: 'USA',
+      type: 'Entire home/apt',
+      guests:'4',
+      bedrooms: '1',
+      beds:'2',
+      reviews:'74',
+      price: '$139',
+      baths:'1',
+      saves: '634'
+    };
 
 
     $scope.likes = [
         'cozy', 'residential', 'close to public transport', 'quiet', 'walkable'
-        ]
+        ];
 
     $scope.comments = [
         'Beautiful 1 bedroom, 1 bathroom private apartment. Private entrance on side. Free off-street parking.',
         'Brand new remodeled bathroom',
         '1 block from subway and lots restaurants/bar walking distance'
-        ]
-
-    $scope.prices = [
-          {cleaningFee: '30', name: 'Yann', bedrooms: 'search'}
         ];
 
+    $scope.safetyFeatures = [
+            'Smoke Detector',
+            'Carbon Monoxide Detector',
+            'First Aid Kit',
+            'Safety Card',
+            'Fire Extinguisher'
+    ];
 
-     $scope.currencies = [
-          {USD: '$', name: '€'}
-        ];
+    $scope.prices = {
+          cleaningFee: '30',
+          name: 'Yann',
+          bedrooms: 'search'
+        };
+
+
+     $scope.currencies = {
+          USD: '$',
+          name: '€'
+        };
+
+    $scope.icons = {
+      star: 'star',
+      starHalf: 'star-half'
+
+    };
 
 
 });
