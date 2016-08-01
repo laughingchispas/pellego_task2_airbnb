df30efc8d4fd8d3b0a2b025b782ba577d31afa70
Nicole Miller <laughingchispas@gmail.com>
2016-08-01 22:21:10 +0200

    Finishing touches Airbnb

--- scripts/angularApp.js
+++ scripts/angularApp.js
@@ -2,27 +2,80 @@ var myApp = angular.module('angularApp', []);
 
 myApp.controller('airbnbController', function($scope) {
 
+    $scope.comments = [
+        {name: 'Richard', image: '14', date: 'June 2016',
+                comment: 'Wow. Immaculately Clean. Great bed. Fridge stocked with soda and beer (complimentary). Assorted teas. And did I mention the bed? OMG. It was amazing. Thanks, Yann.'},
+        {name: 'Fache', image:'13', date: 'June 2016',
+                comment: 'Great stay. Yann was very helpful and accommodating, and parking was very easy, which is a big plus in SF. The neighborhood is very quiet, and there are some lovely coffee shops on the high street just two blocks away.'},
+        {name: 'Fanne', image: '12', date: 'June 2016',
+                comment:'We had an overall nice experience staying with Yann who was kind and responsive when we had questions and requests. We booked it for 4 people and I would say it is too small, It is better for 2. You will have to order food most likely as the place is too small to do basic cooking. As'}
+    ];
+
+    $scope.currencies = {
+          USD: '$',
+          name: '€'
+    };
+
+    $scope.host = {
+        image: '15',
+        name: 'Yann',
+        responseTime: '1 hour',
+        responseRate: '100%',
+        memberSince: 'March 2014',
+        ratings: '157',
+        reference: '1',
+        reviewsOtherProps: '68',
+        tagLine: 'Hi! Bonjour! Oi! Konichiwa!',
+        presentationText: 'I am originally from Montreal Canada (quebecois) and been living in this beautiful city SF for the last few years. I lived in 6 different countries and visited numerous (sorry stopped counting). I like meeting people from all over the world. '
+    };
+
+    $scope.hostPropertyPhotos = [
+        '03',
+        '06',
+        '16',
+        '02',
+        '03'
+    ];
+
+    $scope.icons = {
+        star: 'star',
+        starHalf: 'star-half'
+    };
+
+    $scope.keyWords = [
+        'Beautiful 1 bedroom, 1 bathroom private apartment. Private entrance on side. Free off-street parking.',
+        'Brand new remodeled bathroom',
+        '1 block from subway and lots restaurants/bar walking distance'
+    ];
+
+    $scope.likes = [
+        'cozy', 'residential', 'close to public transport', 'quiet', 'walkable'
+    ];
 
 //navigation link content
     $scope.navCards = [
-      {page: 'Help'},
-      {page: 'Sign Up'},
-      {page: 'Log In'}
+        {page: 'Help'},
+        {page: 'Sign Up'},
+        {page: 'Log In'}
     ];
 
-
-    $scope.users = {
-      image: 'image',
-      name: 'Yann',
-      bedrooms: 'search'
+    $scope.prices = {
+          cleaningFee: '30',
+          name: 'Yann',
+          bedrooms: 'search',
+          extraPeople: '20',
+          extraPeopleAfter: '3',
+          weeklyDiscount: '0%',
+          monthlyDiscount: '0%',
+          cancellation: 'Flexible',
     };
-
 
     $scope.properties = {
       description: 'Private flat, with parking and yard',
       city: 'San Francisco',
       state: 'CA',
       country: 'USA',
+      countryFull: 'United States',
       type: 'Entire home/apt',
       guests:'4',
       bedrooms: '1',
@@ -30,45 +83,52 @@ myApp.controller('airbnbController', function($scope) {
       reviews:'74',
       price: '$139',
       baths:'1',
-      saves: '634'
+      saves: '634',
+      roomType: 'Entire home/apt',
+      propertyType: 'Apartment',
+      minStay: '1 night',
+      from: 'Aug 21, 2016 - Aug 05 2017',
+      minimum:'3 nights'
     };
 
-
-    $scope.likes = [
-        'cozy', 'residential', 'close to public transport', 'quiet', 'walkable'
-        ];
-
-    $scope.comments = [
-        'Beautiful 1 bedroom, 1 bathroom private apartment. Private entrance on side. Free off-street parking.',
-        'Brand new remodeled bathroom',
-        '1 block from subway and lots restaurants/bar walking distance'
-        ];
+    $scope.rules = [
+        'Please respect quiet hours from 11PM to 7AM',
+        'Also, please avoid making noise in the hallway or side of the house. The neighbors can hear that noise and are very picky about noise. Sorry about this and thanks for your understanding.'
+     ];
 
     $scope.safetyFeatures = [
-            'Smoke Detector',
-            'Carbon Monoxide Detector',
-            'First Aid Kit',
-            'Safety Card',
-            'Fire Extinguisher'
+        'Smoke Detector',
+        'Carbon Monoxide Detector',
+        'First Aid Kit',
+        'Safety Card',
+        'Fire Extinguisher'
     ];
 
-    $scope.prices = {
-          cleaningFee: '30',
-          name: 'Yann',
-          bedrooms: 'search'
-        };
+    $scope.similarHousing = [
+        'Houses',
+        'Bed and Breakfasts',
+        'Lofts',
+        'Villas',
+        'Condominiums'
+    ];
 
+    $scope.similarListings = [
+        {propertyName: 'Ingleside Hideaway', image: '04', propertyType: 'June 2016', hostImage:'10', price: '$169', description:'Entire Home/Apt', distanceAway:'2.0 mi'},
+        {propertyName: 'Newly Renovated, Modern B...', image:'05', propertyType: 'June 2016', hostImage:'09', price: '$158', description:'Entire Home/Apt', distanceAway:'3.3 mi'},
+        {propertyName: 'Cozy 2bd/1ba, outer Missio...', image: '08', propertyType: 'June 2016', hostImage:'11', price: '$143', description:'Entire Home/Apt', distanceAway:'2.4 mi'}
+    ];
 
-     $scope.currencies = {
-          USD: '$',
-          name: '€'
-        };
-
-    $scope.icons = {
-      star: 'star',
-      starHalf: 'star-half'
-
-    };
-
-
+    $scope.similarLocation = [
+        'Sonoma Vaction Rentals',
+        'Petaluma Veaction Rentals',
+        'Berkeley Vacation Rentals',
+        'Santa Cruz Vacation Rentals',
+        'San Francisco',
+        'San Jose',
+        'Carmel',
+        'Sunnyvale',
+        'Oakland',
+        'Menlo Park',
+        'San Mateo'
+    ];
 });
